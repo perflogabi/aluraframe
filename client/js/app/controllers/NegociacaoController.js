8f3bc1ad669ea9fe2087dca1898202a3d65f7133
Gabriela Florêncio <perflogabi@gmail.com>
2022-08-15 22:02:17 -0300

    segundo commit

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,23 +6,31 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes()
     }
 
     adicionar(event) {
+
         event.preventDefault();
 
-        let data = new Date(...this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
+        this._listaNegociacoes.adicionar(this._criaNegociacao());
+        this._limpaFormulario();
+        console.log(this._listaNegociacoes.negociacoes);
 
-        );
+        _criaNegociacao() {
+            return new Negociacao(
+                DateHelper.textoParaData(this._inputData.value),
+                this._inputQuantidade.value,
+                this._inputValor.value),
+        }
 
-        let negociacao = new Negociacao(
-            data,
-            this._inputQuantidade.value,
-            this._inputValor.value
-        );
+        _limpaFormulario() {
 
-        console.log(negociacao);
-    };
+            this._inputData.value = '';
+            this._inputQuantidade.value = 1;
+            this._inputValor.value = 0.0;
+            this._inuputData.focus();
+        }
+
+    }
 }
