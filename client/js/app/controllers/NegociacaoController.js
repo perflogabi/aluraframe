4a0c4cd0cca4d4c509ba2476f04f93f97334cef0
Gabriela Florêncio <perflogabi@gmail.com>
2022-08-16 21:17:13 -0300

    terceiro commit

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,30 +7,34 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes()
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._negociacoesView.update();
     }
 
-    adicionar(event) {
+    adiciona(event) {
 
         event.preventDefault();
 
-        this._listaNegociacoes.adicionar(this._criaNegociacao());
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._limpaFormulario();
-        console.log(this._listaNegociacoes.negociacoes);
+    }
 
-        _criaNegociacao() {
-            return new Negociacao(
-                DateHelper.textoParaData(this._inputData.value),
-                this._inputQuantidade.value,
-                this._inputValor.value),
-        }
+    _criaNegociacao() {
 
-        _limpaFormulario() {
-
-            this._inputData.value = '';
-            this._inputQuantidade.value = 1;
-            this._inputValor.value = 0.0;
-            this._inuputData.focus();
-        }
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);
 
     }
+
+    _limpaFormulario() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
 }
